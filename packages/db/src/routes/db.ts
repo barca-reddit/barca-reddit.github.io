158379da71db8d1d11e7d3d7c773035b7fed44ed
virtuallyunknown <dechobarca@gmail.com>
2025-07-05 02:48:05 +0300

    feat(db): add devvit-sources api endpoint

--- packages/db/src/routes/db.ts
+++ packages/db/src/routes/db.ts
@@ -1,4 +1,4 @@
-import { dbMetaSchema, dbSourceListSchema, dbUpdateListSchema } from '@repo/schemas';
+import { dbMetaSchema, dbSourceListSchema, dbUpdateListSchema, devvitBaseListSchema } from '@repo/schemas';
 import camelCaseKeys from 'camelcase-keys';
 import { Hono } from 'hono';
 
@@ -8,6 +8,21 @@ router.get('/sources', async (c) => {
     const res = await c.env.DB.prepare('SELECT * FROM sources_list;').all();
 
     const data = dbSourceListSchema.safeParse(
+        camelCaseKeys(res.results, { deep: true }),
+    );
+
+    if (!data.success) {
+        console.error(JSON.stringify(data.error.format(), null, 2));
+        return c.json({ success: false });
+    }
+
+    return c.json(data.data);
+});
+
+router.get('/devvit-sources', async (c) => {
+    const res = await c.env.DB.prepare('SELECT * FROM devvit_sources_list;').all();
+
+    const data = devvitBaseListSchema.safeParse(
         camelCaseKeys(res.results, { deep: true }),
     );
 
