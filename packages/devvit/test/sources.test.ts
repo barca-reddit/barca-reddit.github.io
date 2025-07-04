96ea492ea74dc9649df094247f39c9109ec6e624
virtuallyunknown <dechobarca@gmail.com>
2025-07-05 02:49:29 +0300

    test(devvit): update remote url for fetching sources

--- packages/devvit/test/sources.test.ts
+++ packages/devvit/test/sources.test.ts
@@ -6,17 +6,11 @@ describe('zod schema safeParse', () => {
     let sourceList: unknown;
 
     beforeAll(async () => {
-        const res = await fetch('https://media-reliability.barcareddit.workers.dev/db/sources');
+        const res = await fetch('https://media-reliability.barcareddit.workers.dev/db/devvit-sources');
         sourceList = await res.json();
     });
 
     test('sources list json', () => {
-        /**
-         * Converting the source list to string is kinda pointless because
-         * because the import converts it to an object anyway.
-         * 
-         * But this is only done for test purposes here so it's fine.
-         */
         const result = devvitSettings
             .shape
             .sources
